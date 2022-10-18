c76de77f5e3a317a3bd0240a512d46aa1b559739
ktran1005 <kdt57@drexel.edu>
2022-10-17 21:30:30 -0400

    new version

--- Week1/mergeS.cpp
+++ Week1/mergeS.cpp
@@ -64,45 +64,60 @@ void mergesort(int a[], int size) {
 
 }
 
-void mergeAllSections(int a[],int size ,std::vector <int> startIndexes, std::vector <int> stopIndexes, int numThreads) {
-	//for (int i = 0; i < numThreads; i++) {
-	//	int left = startIndexes[i];
-	//	int right = stopIndexes[;
-	//	int middle = left + (right - left) / 2;
-	//	merge(a, left, middle, right);
-	//	msort(a, left, right);
-	//}
-	std::vector<int> temp1, temp2;
-
+void mergeAllSections(int a[],int size ,int* startIndexes, int* stopIndexes, int numThreads) {
+	std::thread* threads = new std::thread[32];
 	int n1 = (stopIndexes[0] - startIndexes[0]) + 1;
 	int n2 = (stopIndexes[1] - startIndexes[1]) + 1;
-	
+	int* n3 = new int[n1+n2]{0};
+	int* new_start_idx = new int[10];
+	int* new_stop_idx = new int[10];
+
 	int mid_1 = (stopIndexes[0] - startIndexes[0]) / 2;
 	int mid_2 = binarySearch(a[mid_1], a, startIndexes[1], stopIndexes[1]);
-	
-	std::cout << "The middle point of sorted list " << a[mid_1] << "\n";
-	std::cout << "the idx of midpoint from sorted list 1 at sorted list 2 is: " << mid_2 << std::endl;
-	for (int i=0; i < mid_1; i++)
-		temp1.push_back(a[i]);
-	int k = startIndexes[1];
-	while (a[k] != mid_2) {
-		temp1.push_back(a[k]);
-		k++;
-	}
-	int l = mid_2 + 1;
-	for (int i=mid_1+1; i <= stopIndexes[0]; i++)
-		temp2.push_back(a[i]);
-	for (int j=mid_2+1; j <= stopIndexes[1]; j++)
-		temp2.push_back(a[j]);
-	int* n3 = new int[n1 + n2];
 
-	for (int first=0; first < temp1.size(); first++)
-		std::cout << temp1[first] << " ";
-	std::cout << "\n";
-	for (int second=0; second < temp2.size(); second++)
-		std::cout << temp2[second] << " ";
-	std::cout << "\n";
-	//mergesort(temp1, temp1.size());
+	int q3 = (mid_1 - startIndexes[0]) + ((mid_2 + 1) -startIndexes[1]);
+	n3[q3] = a[mid_1];
+	std::cout << "q3: " << q3 << " n3[q3]: " <<n3[q3] << "\n";
+	int n = 0;
+
+	for (int i=0; i < mid_1; i++) {
+		n3[i] = a[i];
+		n++;
+	}
+
+	for (int k= startIndexes[1]; k <= mid_2; k++) {
+		n3[n] = a[k];
+		n++;
+	}
+	if (n == q3)
+		n++;
+	std::cout << "n: " << n << "\n";
+
+	for (int i= mid_1 + 1; i <= stopIndexes[0]; i++) {
+		n3[n] = a[i];
+		n++;
+	}
+
+	for (int j=mid_2+1; j <= stopIndexes[1]; j++) {
+		n3[n] = a[j];
+		n++;
+	}
+	new_start_idx[0] = 0;
+	new_stop_idx[1] = 3;
+	new_start_idx[0] = 5;
+	new_stop_idx[1] = 9;
+
+	for (int i=0; i < numThreads; i++) {
+		threads[i] = std::thread(msort, n3, new_start_idx[i], new_stop_idx[i]); 
+	}
+
+	for (int i=0; i < numThreads; i++) {
+		threads[i].join();
+	}
+	for (int i=0; i < size; i++)
+		a[i] = n3[i];
+	
+
 }
 
 
@@ -112,21 +127,23 @@ void tmergesort(int a[], int size, int numThreads) {
 		return;
 	}
 
-	std::vector <int> start_idx, stop_idx;
-	std::vector <std::thread> threads;
+	std::thread *threads = new std::thread[32];
 	int number_per_thread = size / numThreads;
+	int* start_idx = new int[number_per_thread]{0};
+	int* stop_idx = new int[number_per_thread]{0};
 	std::cout << "Number per threads: " << number_per_thread << std::endl;
 	for (int i=0; i < numThreads; i++) {
 		int start = i * number_per_thread;
 		int end = (i + 1) * number_per_thread - 1;
-		start_idx.push_back(start);
-		stop_idx.push_back(end);
+		start_idx[i] = start;
+		stop_idx[i] = end;
 	}
 
 	for (int i = 0; i < numThreads; i++) {
-		threads.push_back(std::thread(msort,a, start_idx[i], stop_idx[i]));
+		threads[i] = std::thread(msort, a, start_idx[i], stop_idx[i]);
 	}
   
+	/*
 	for (int f:start_idx) {
 				std::cout << f << " ";
 	}
@@ -136,9 +153,13 @@ void tmergesort(int a[], int size, int numThreads) {
 
 	}
     std::cout << std::endl; 
+	*/
 	for (int i=0; i < numThreads;i++) {
 		threads[i].join();
 	}
+	// for (int i=0; i < 10; i++)
+	// 	std::cout << a[i] << " ";
+	// std::cout << "\n";
 
     mergeAllSections(a, size ,start_idx, stop_idx, numThreads);
 
